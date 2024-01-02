97512afcf138c588983afb8badf085b483dc4b85
MordechaiAmiram <126167105+MordechaiAmiram@users.noreply.github.com>
2024-01-02 16:27:42 +0200

    fixed bug in the review component

--- server/client/src/components/review/reviewForm.js
+++ server/client/src/components/review/reviewForm.js
@@ -4,10 +4,12 @@ import React from 'react'
 function ReviewForm({ review, handleDelete }) {
     const { firstName, lastName, rating, text, date } = review
     const isAdmin = JSON.parse(localStorage.getItem('currentUser'))?.isAdmin
-    
+
     return (
         <>
-            {isAdmin && <Button onClick={handleDelete}>מחק</Button>}
+            {isAdmin === true &&
+                <Button onClick={handleDelete}>מחק</Button>
+            }
             <div>{`נכתבה על ידי: ${firstName} ${lastName}`} <br />
                 {`נכתבה בתאריך: ${date}`} <br />
                 {text} <br />
