138f0bf34b4fcfb13be0e52f28a5748166551ed7
MordechaiAmiram <126167105+MordechaiAmiram@users.noreply.github.com>
2024-01-07 13:48:55 +0200

    add style to slidebar

--- server/client/src/components/sideBar/SideBarForm.js
+++ server/client/src/components/sideBar/SideBarForm.js
@@ -6,32 +6,17 @@ import SearchField from '../searchField/SearchField'
 
 function SideBarForm({ profile }) {
   return (
-    <div className='side-bar'>
-      {!profile &&
-        <>
-          <div className='header'>
-            {/* <div className='hello'>
-              שלום אורח
-            </div> */}
-            {/* <div className='nav-item'>
-              <RouterLink className='link'
-                to={'/sign-up'}>הרשמה</RouterLink></div> */}
-            {/* <div className='nav-item'>
-              <RouterLink className='link'
-                to={'/log-in'}>כניסה</RouterLink>
-            </div> */}
-          </div>
-          <div className='search-side-bar'><SearchField /></div>
-          {/* <div className='left-bar'>
-            <div className='nav-item'>
-              <RouterLink className='link home-link'
-                to={'/'}>Freeלאנ"ש</RouterLink>
-            </div> 
-          </div> */}
-          <div className='slider'><DensityMediumRoundedIcon /></div>
-        </>
-      }
-    </div>
+    <>
+      <div className='side-bar'>
+        <div className='header-title'>
+          <h3><RouterLink className='link home-link-side-bar'
+            to={'/'}>Freeלאנ"ש</RouterLink></h3>
+        </div>
+        <div className='search-side-bar'><SearchField /></div>
+
+        <div className='slider-btn'><DensityMediumRoundedIcon /></div>
+      </div>
+    </>
   )
 }
 
