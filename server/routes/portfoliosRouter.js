0fc7482f282adbad1060ea18a998be82a3ff52c1
MordechaiAmiram <126167105+MordechaiAmiram@users.noreply.github.com>
2024-01-11 15:25:25 +0200

    add portfolios router

---
+++ server/routes/portfoliosRouter.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const { getFreelancePortfolios } = require('../dbOperations/portfolios')
+
+router
+    .route('/')
+    .get(async (req, res) => {
+        try {
+            const portfolios = await getFreelancePortfolios(req.params.freelancId)
+            if (portfolios) {
+                res.status(200)
+                    .send(portfolios)
+            } else {
+                res.status(400)
+                    .send('Bad request')
+            }
+        } catch (err) {
+            console.error(err.message)
+            res.status(500)
+        }
+    })
