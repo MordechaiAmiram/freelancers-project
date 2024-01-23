b98a69f73ad4cc3667bdc978750a3ef0f295858a
MordechaiAmiram <126167105+MordechaiAmiram@users.noreply.github.com>
2024-01-23 12:36:06 +0200

    remove descendingComparator component

--- server/client/src/components/selectAndSortTable/descendingComparator.js
+++
@@ -1,9 +0,0 @@
-export default function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
