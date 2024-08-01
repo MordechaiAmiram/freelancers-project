8b403959ca60bec61078b4c3991beebd59c1ae38
Modechai A <moti5865@gmail.com>
2024-08-01 11:36:23 +0300

    Fix the base url be updated in the token dynamcally

--- client/src/services/BaseURL.js
+++ client/src/services/BaseURL.js
@@ -1,14 +1,24 @@
-import axios from "axios";
-
-const token = localStorage.getItem('accessToken')
+import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development' ?
         'http://localhost:4000/api' :
-        '/api',
-        headers: {
-            Authorization: token ? `Bearer ${token}` : ''
-        }
+        '/api'
 })
+
+api.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('accessToken')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        } else {
+            delete config.headers.Authorization
+        }
+        return config
+    },
+    error => {
+        return Promise.reject(error)
+    }
+)
 
 export default api
